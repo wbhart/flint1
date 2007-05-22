7dc0055d5cea970b77d8c5024bbad6d84daa4c0f
dmharvey <dmharvey@2531bd8e-7522-0410-9704-d59f193d18de>
2007-05-22 02:18:57 +0000

    added ssfft profiling code to ZmodFpoly profiling, for comparison purposes
    git-svn-id: https://flint.svn.sourceforge.net/svnroot/flint/trunk@522 2531bd8e-7522-0410-9704-d59f193d18de

--- ZmodFpoly-profile.c
+++ ZmodFpoly-profile.c
@@ -162,4 +162,78 @@ void prof2dDriver_ZmodFpoly_IFFT(char* params)
 }
 
 
+// ============================================================================
+
+
+void sample_ssfft(unsigned long length, unsigned long n, unsigned long count)
+{
+   unsigned long m = ceil_log2(2*length);
+   
+   ZmodFpoly_t poly;
+   ZmodFpoly_init(poly, m, n, 1);
+   poly->length = length;
+   
+   // todo: need to generate random data here
+   
+   prof2d_start();
+
+   for (unsigned long i = 0; i < count; i++)
+      ssfft_fft(poly->coeffs, 1, m, length, 2*length, 0,
+                (4*n*FLINT_BITS_PER_LIMB) >> m, n, poly->scratch);
+
+   prof2d_stop();
+   
+   ZmodFpoly_clear(poly);
+}
+
+
+char* prof2dDriverString_ssfft(char* params)
+{
+   return "legacy ssfft code";
+}
+
+
+/*
+Parameters for this target are:
+   length_min: minimum truncation length
+   length_max: maximum truncation length
+   length_ratio: e.g. 1.03 means increase by 3% at a time
+   n_count: number of coefficient lengths to test
+*/
+void prof2dDriver_ssfft(char* params)
+{
+   int length_min, length_max, n_count;
+   double length_ratio;
+
+   if (strlen(params) == 0)
+   {
+      // default parameters:
+      length_min = 100;
+      length_max = 200;
+      length_ratio = 1.1;
+      n_count = 6;
+   }
+   else
+   {
+      sscanf(params, "%d %d %lf %d", &length_min, &length_max,
+                                     &length_ratio, &n_count);
+   }
+
+   prof2d_set_sampler(sample_ssfft);
+
+   for (unsigned long length = length_min; length < length_max;
+        length = (int)(ceil(length_ratio * length)))
+   {
+      unsigned long m = ceil_log2(2*length);
+
+      unsigned long n_skip = (1 << m) / (4*FLINT_BITS_PER_LIMB);
+      if (n_skip == 0)
+         n_skip = 1;
+
+      for (unsigned long n = n_skip; n <= n_count * n_skip; n += n_skip)
+         prof2d_sample(length, n);
+   }
+}
+
+
 // end of file ****************************************************************
