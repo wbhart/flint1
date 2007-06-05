b98d5251685dd6cddbc2ecbd08e0d1b9c5a3adbd
wbhart <wbhart@2531bd8e-7522-0410-9704-d59f193d18de>
2007-06-05 02:24:54 +0000

    New memory manager files.
    git-svn-id: https://flint.svn.sourceforge.net/svnroot/flint/trunk@613 2531bd8e-7522-0410-9704-d59f193d18de

---
+++ memory-manager.h
@@ -0,0 +1,28 @@
+/****************************************************************************
+
+memory-manager.h: FLINT-wide memory management
+
+Copyright (C) 2007, William Hart and David Harvey
+
+*****************************************************************************/
+
+#ifndef FLINT_MANAGER_H
+#define FLINT_MANAGER_H
+
+void* flint_stack_alloc(unsigned long length);
+
+void* flint_stack_alloc_bytes(unsigned long bytes);
+
+void flint_stack_release();
+
+void* flint_heap_alloc(unsigned long limbs);
+
+void* flint_heap_alloc_bytes(unsigned long bytes);
+
+void* flint_heap_realloc(void* block, unsigned long limbs);
+
+void* flint_heap_realloc_bytes(void* block, unsigned long limbs);
+
+void flint_heap_free(void* block);
+
+#endif
