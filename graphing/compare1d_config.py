815c26181b2d10a0cc68ff017a6471ea38e9df88
dmharvey <dmharvey@2531bd8e-7522-0410-9704-d59f193d18de>
2007-06-28 03:29:55 +0000

    remove flags from 1d config file which don't make sense there
    git-svn-id: https://flint.svn.sourceforge.net/svnroot/flint/trunk@729 2531bd8e-7522-0410-9704-d59f193d18de

--- graphing/compare1d_config.py
+++ graphing/compare1d_config.py
@@ -18,12 +18,6 @@ CONFIG_dpi = 96
 
 
 #------------------------------------------------------------------------------
-# radius of each dot (don't ask me what the units are, I have no idea)
-
-CONFIG_dotsize = 3
-
-
-#------------------------------------------------------------------------------
 # title at the top of the image
 
 CONFIG_title = None
@@ -49,24 +43,6 @@ CONFIG_yscale = None
 
 CONFIG_xlabel = None
 CONFIG_ylabel = None
-
-
-#------------------------------------------------------------------------------
-# if the values from the two data sets differ by a factor of more than
-# CONFIG_truncate, the maximum colour intensity is assigned
-
-CONFIG_truncate = 2.0
-
-
-#------------------------------------------------------------------------------
-# The minimum colour intensity, in the range (0, 1).
-# If this is zero, then negative through positive values are plotted on
-# a continuous spectrum from blue through red. If this is 0.5, then a tiny
-# positive value is already somewhat brown, and a tiny negative value is
-# already somewhat blue. If this is 1.0, you get all flat blue and all flat
-# red.
-
-CONFIG_min_intensity = 0.5
 
 
 ################# end of config file
