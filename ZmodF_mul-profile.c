2db6f9437e8d4e300e864ba38e0bf05ea02b1e9c
dmharvey <dmharvey@2531bd8e-7522-0410-9704-d59f193d18de>
2007-06-29 21:39:21 +0000

    rewrite ZmodF_mul-profile to use new argument passing; also separate out the different algorithms into different targets, and temporarily removing profiling for squaring code
    git-svn-id: https://flint.svn.sourceforge.net/svnroot/flint/trunk@740 2531bd8e-7522-0410-9704-d59f193d18de

--- ZmodF_mul-profile.c
+++ ZmodF_mul-profile.c
@@ -15,28 +15,12 @@ Copyright (C) 2007, William Hart and David Harvey
 #include <math.h>
 
 
-// ============================================================================
-
-
-// yuck, need to make this a global, something wrong with the design...
-unsigned long ZmodF_mul_depth = 0;
-
-
-void sample_ZmodF_mul(unsigned long n, void* arg, unsigned long count)
+/*
+This is a helper function used by the other sampler functions below.
+*/
+void sample_ZmodF_mul_helper(ZmodF_mul_info_t info, unsigned long n,
+                             unsigned long count)
 {
-   ZmodF_mul_info_t info;
-   
-   // this function assumes n is legal for the requested algorithm
-   
-   if (ZmodF_mul_depth == 0)
-      ZmodF_mul_info_init(info, n, 0);
-   else if (ZmodF_mul_depth == 1)
-      ZmodF_mul_info_init_plain(info, n, 0);
-   else if (ZmodF_mul_depth == 2)
-      ZmodF_mul_info_init_threeway(info, n, 0);
-   else
-      ZmodF_mul_info_init_negacyclic(info, n, ZmodF_mul_depth, 0);
-
    mp_limb_t* x1 = (mp_limb_t*) malloc((n+1) * sizeof(mp_limb_t));
    mp_limb_t* x2 = (mp_limb_t*) malloc((n+1) * sizeof(mp_limb_t));
    mp_limb_t* x3 = (mp_limb_t*) malloc((n+1) * sizeof(mp_limb_t));
@@ -53,128 +37,186 @@ void sample_ZmodF_mul(unsigned long n, void* arg, unsigned long count)
 
    prof_stop();
 
-   ZmodF_mul_info_clear(info);
    free(x3);
    free(x2);
    free(x1);
 }
 
 
-char* profDriverString_ZmodF_mul(char* params)
+// ============================================================================
+
+
+void sample_ZmodF_mul_plain(unsigned long n, void* arg, unsigned long count)
+{
+   ZmodF_mul_info_t info;
+   ZmodF_mul_info_init_plain(info, n, 0);
+   sample_ZmodF_mul_helper(info, n, count);
+   ZmodF_mul_info_clear(info);
+}
+
+
+char* profDriverString_ZmodF_mul_plain(char* params)
 {
    return
-   "ZmodF_mul over various n and multiplication algorithms.\n"
-   "Parameters: n_min, n_max, n_skip, depth. Depth == 0 means\n"
-   "select algorithm automatically. Depth == 1 means use plain algorithm.\n"
-   "Depth == 2 means use threeway algorithm. Otherwise negacyclic algorithm\n"
-   "is used with indicated depth.\n";
+   "ZmodF_mul using plain algorithm.\n"
+   "Parameters: n_min, n_max, n_skip.\n";
 }
 
 
-char* profDriverDefaultParams_ZmodF_mul()
+char* profDriverDefaultParams_ZmodF_mul_plain()
 {
-   return "20 1000 1 4";
+   return "1 1000 1";
 }
 
 
-void profDriver_ZmodF_mul(char* params)
+void profDriver_ZmodF_mul_plain(char* params)
 {
-   unsigned long n_min, n_max, n_skip, depth;
+   unsigned long n_min, n_max, n_skip;
+   sscanf(params, "%ld %ld %ld", &n_min, &n_max, &n_skip);
 
-   sscanf(params, "%ld %ld %ld %ld", &n_min, &n_max, &n_skip, &depth);
-
-   prof1d_set_sampler(sample_ZmodF_mul);
+   prof1d_set_sampler(sample_ZmodF_mul_plain);
    
-   ZmodF_mul_depth = depth;
    for (unsigned long n = n_min; n <= n_max; n += n_skip)
-   {
-      // ensure n is legal for given algorithm
-      if ((depth == 2) && (n % 3))
-         continue;
-      if (depth > 2 && ((n * FLINT_BITS) & ((1 << depth) - 1)))
-         continue;
-      
       prof1d_sample(n, NULL);
-   }
 }
-
 
 
 
 // ============================================================================
 
 
-void sample_ZmodF_sqr(unsigned long n, void* arg, unsigned long count)
+void sample_ZmodF_mul_threeway(unsigned long n, void* arg, unsigned long count)
 {
    ZmodF_mul_info_t info;
-   
-   // this function assumes n is legal for the requested algorithm
-
-   if (ZmodF_mul_depth == 0)
-      ZmodF_mul_info_init(info, n, 1);
-   else if (ZmodF_mul_depth == 1)
-      ZmodF_mul_info_init_plain(info, n, 1);
-   else if (ZmodF_mul_depth == 2)
-      ZmodF_mul_info_init_threeway(info, n, 1);
-   else
-      ZmodF_mul_info_init_negacyclic(info, n, ZmodF_mul_depth, 1);
-
-   mp_limb_t* x1 = (mp_limb_t*) malloc((n+1) * sizeof(mp_limb_t));
-   mp_limb_t* x3 = (mp_limb_t*) malloc((n+1) * sizeof(mp_limb_t));
-
-   profiler_random_limbs(x1, n);
-   x1[n] = 0;
-   
-   prof_start();
-
-   for (unsigned long i = 0; i < count; i++)
-      ZmodF_mul_info_sqr(info, x3, x1);
-
-   prof_stop();
-
+   ZmodF_mul_info_init_threeway(info, n, 0);
+   sample_ZmodF_mul_helper(info, n, count);
    ZmodF_mul_info_clear(info);
-   free(x3);
-   free(x1);
 }
 
 
-char* profDriverString_ZmodF_sqr(char* params)
+char* profDriverString_ZmodF_mul_threeway(char* params)
 {
    return
-   "ZmodF_sqr over various n and multiplication algorithms.\n"
-   "Parameters: n_min, n_max, n_skip, depth. Depth == 0 means\n"
-   "select algorithm automatically. Depth == 1 means use plain algorithm.\n"
-   "Depth == 2 means use threeway algorithm. Otherwise negacyclic algorithm\n"
-   "is used with indicated depth.\n";
+   "ZmodF_mul using threeway algorithm.\n"
+   "Parameters: n_min, n_max, n_skip.\n"
+   "Note: n not divisible by 3 are skipped.\n";
 }
 
 
-char* profDriverDefaultParams_ZmodF_sqr()
+char* profDriverDefaultParams_ZmodF_mul_threeway()
 {
-   return "20 1000 1 4";
+   return "1 1000 1";
 }
 
 
-void profDriver_ZmodF_sqr(char* params)
+void profDriver_ZmodF_mul_threeway(char* params)
 {
-   unsigned long n_min, n_max, n_skip, depth;
+   unsigned long n_min, n_max, n_skip;
+   sscanf(params, "%ld %ld %ld", &n_min, &n_max, &n_skip);
 
-   sscanf(params, "%ld %ld %ld %ld", &n_min, &n_max, &n_skip, &depth);
-
-   prof1d_set_sampler(sample_ZmodF_sqr);
+   prof1d_set_sampler(sample_ZmodF_mul_threeway);
    
-   ZmodF_mul_depth = depth;
+   // round up n_min so we start on a permissible value
+   while (n_min % 3)
+      n_min++;
+   
    for (unsigned long n = n_min; n <= n_max; n += n_skip)
    {
-      // ensure n is legal for given algorithm
-      if ((depth == 2) && (n % 3))
-         continue;
-      if (depth > 2 && ((n * FLINT_BITS) & ((1 << depth) - 1)))
-         continue;
-      
-      prof1d_sample(n, NULL);
+      if (n % 3 == 0)
+         prof1d_sample(n, NULL);
    }
 }
+
+
+// ============================================================================
+
+
+/*
+arg should point to an unsigned long, which is the transform depth
+*/
+void sample_ZmodF_mul_negacyclic(unsigned long n, void* arg,
+                                 unsigned long count)
+{
+   unsigned long depth = * (unsigned long*) arg;
+   ZmodF_mul_info_t info;
+   ZmodF_mul_info_init_negacyclic(info, n, depth, 0);
+   sample_ZmodF_mul_helper(info, n, count);
+   ZmodF_mul_info_clear(info);
+}
+
+
+char* profDriverString_ZmodF_mul_negacyclic(char* params)
+{
+   return
+   "ZmodF_mul using negacyclic algorithm.\n"
+   "Parameters: depth, n_min, n_max, n_skip.\n"
+   "Note: those n not supported for the given depth are skipped.\n";
+}
+
+
+char* profDriverDefaultParams_ZmodF_mul_negacyclic()
+{
+   return "4 1 1000 1";
+}
+
+
+void profDriver_ZmodF_mul_negacyclic(char* params)
+{
+   unsigned long depth, n_min, n_max, n_skip;
+   sscanf(params, "%ld %ld %ld %ld", &depth, &n_min, &n_max, &n_skip);
+
+   prof1d_set_sampler(sample_ZmodF_mul_negacyclic);
+
+   // round up n_min so we start on a permissible value
+   while ((n_min * FLINT_BITS) & ((1 << depth) - 1))
+      n_min++;
+   
+   for (unsigned long n = n_min; n <= n_max; n += n_skip)
+   {
+      if (((n * FLINT_BITS) & ((1 << depth) - 1)) == 0)
+         prof1d_sample(n, &depth);
+   }
+}
+
+
+
+// ============================================================================
+
+
+void sample_ZmodF_mul_auto(unsigned long n, void* arg, unsigned long count)
+{
+   ZmodF_mul_info_t info;
+   ZmodF_mul_info_init(info, n, 0);
+   sample_ZmodF_mul_helper(info, n, count);
+   ZmodF_mul_info_clear(info);
+}
+
+
+char* profDriverString_ZmodF_mul_auto(char* params)
+{
+   return
+   "ZmodF_mul using automatically selected algorithm.\n"
+   "Parameters: n_min, n_max, n_skip.\n";
+}
+
+
+char* profDriverDefaultParams_ZmodF_mul_auto()
+{
+   return "1 1000 1";
+}
+
+
+void profDriver_ZmodF_mul_auto(char* params)
+{
+   unsigned long n_min, n_max, n_skip;
+   sscanf(params, "%ld %ld %ld", &n_min, &n_max, &n_skip);
+
+   prof1d_set_sampler(sample_ZmodF_mul_auto);
+   
+   for (unsigned long n = n_min; n <= n_max; n += n_skip)
+      prof1d_sample(n, NULL);
+}
+
 
 
 // end of file ****************************************************************
