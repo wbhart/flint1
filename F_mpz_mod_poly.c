7212a61688e80eab55c7dd770f9da35375a6ce1e
Andy <andy@andy-laptop.(none)>
2009-11-23 19:13:27 +0100

    New module F_mpz_mod_poly with init and clear

---
+++ F_mpz_mod_poly.c
@@ -0,0 +1,53 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/*****************************************************************************
+
+   F_mpz_mod_poly.c: Polynomials over F_mpz mod p.
+   
+   Copyright (C) 2009, William Hart, Andy Novocin
+
+*****************************************************************************/
+
+#include "F_mpz_mod_poly.h"
+#include "long_extras.h"
+#include "longlong_wrapper.h"
+#include "longlong.h"
+#include "flint.h"
+
+/****************************************************************************
+
+   Initialisation and memory management
+
+****************************************************************************/
+
+void F_mpz_mod_poly_init(F_mpz_mod_poly_t poly, F_mpz_t p)
+{
+   poly->coeffs = (unsigned long*) flint_heap_alloc(1);
+   
+   poly->P = P;
+   poly->alloc = 1;
+   poly->length = 0;
+}
+
+void F_mpz_mod_poly_clear(F_mpz_mod_poly_t poly)
+{
+   flint_heap_free(poly->coeffs);
+}
+
---
+++ F_mpz_mod_poly.h
@@ -0,0 +1,67 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/*****************************************************************************
+
+   F_mpz_mod_poly.h: Polynomials over F_mpz mod p.
+   
+   Copyright (C) 2009, William Hart, Andy Novocin
+
+*****************************************************************************/
+
+#include <gmp.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "flint.h"
+#include "F_mpz_poly.h"
+#include "memory-manager.h"
+#include "mpn_extras.h"
+#include "long_extras.h"
+
+#ifndef _F_MPZ_MOD_POLY_H_
+#define _F_MPZ_MOD_POLY_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+typedef struct
+{
+   unsigned long *coeffs;
+   unsigned long alloc;
+   unsigned long length;
+   F_mpz_t P;
+} F_mpz_mod_poly_struct;
+
+typedef F_mpz_mod_poly_struct F_mpz_mod_poly_t[1];
+
+void F_mpz_mod_poly_init(F_mpz_mod_poly_t poly, F_mpz_t P);
+
+void F_mpz_mod_poly_clear(F_mpz_mod_poly_t poly);
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif /* _F_MPZ_MOD_POLY_H_ */
