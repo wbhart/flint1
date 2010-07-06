b2863e34bbd57cf10bd81034f634085950db7270
Andy Novocin <andy@novocin.com>
2010-07-06 18:38:10 +0200

    added new functions for GSO\n

--- mpq_mat.h
+++ mpq_mat.h
@@ -55,7 +55,32 @@ typedef mpq_mat_struct mpq_mat_t[1];
 void mpq_mat_init(mpq_mat_t mat, ulong r, ulong c);
 void mpq_mat_clear(mpq_mat_t mat);
 
+//------------------------------------------------------
+//Conversion
 
+void mpz_mat_to_mpq_mat(mpq_mat_t res, mpz_mat_t mat);
+
+// ----------------------------------------------------
+// I/O 
+
+void mpq_mat_print(mpq_mat_t mat);
+
+void mpq_mat_print_pretty(mpq_mat_t mat);
+
+// ----------------------------------------------
+//Gram Schmidt Orthogonalization functions
+
+void mpq_mat_row_scalar_product(mpq_mat_t mat1, ulong r1, mpq_mat_t mat2, ulong r2, mpq_t scalar);
+
+void mpq_mat_row_inner_product(mpq_t res, mpq_mat_t mat1, ulong r1, mpq_mat_t mat2, ulong r2);
+
+void mpq_mat_row_add(mpq_mat_t mat1, ulong r1, mpq_mat_t mat2, ulong r2);
+
+// will set mu[i, j] to gso projection for i < j and GS[i] to i'th G-S vector
+void mpq_mat_GS(mpq_mat_t mu, mpq_mat_t GS, mpq_mat_t mat);
+
+// returns 1 if delta-eta reduced 0 otherwise
+int mpq_mat_is_reduced(mpq_mat_t mu, mpq_mat_t GS, double delta, double eta);
 // *************** end of file
 
 #ifdef __cplusplus
