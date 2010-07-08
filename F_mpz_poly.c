062ec177f542406ee93e2cbafe137779a6af8857
Andy Novocin <andy@novocin.com>
2010-07-08 12:11:26 +0200

    trying dumb trick using 7.5 r bits in Hensel comp

--- F_mpz_poly.c
+++ F_mpz_poly.c
@@ -7360,7 +7360,7 @@ printf(" first two clds took %f seconds\n", (double) cld_data_total/ (double) CL
 //Trying to get (a-b)log(p)*(len-2) = .12*r^2 where b = avg_b/log2(p)...
 
       //long n_a = (long) (double)( 0.12 * r * r /(double)( (len - 2) ) + avg_b / log2( (double) p )  );
-      long n_a = (long) (double)(( (3.5)*r / log2((double) p)   + (double) avg_b / log2( (double) p )  ));
+      long n_a = (long) (double)(( (7.5)*r / log2((double) p)   + (double) avg_b / log2( (double) p )  ));
 
       //n_a = (long) pow( (double) 2, ceil( log2( (double) n_a ) ) );
 
