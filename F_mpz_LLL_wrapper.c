19bf4b6e988aeeeb74c9c9eab15c803ac4a7ae21
Andy Novocin <andy@novocin.com>
2010-09-02 18:59:37 +0200

    trying less non-heuristic loops

--- F_mpz_LLL_wrapper.c
+++ F_mpz_LLL_wrapper.c
@@ -115,7 +115,7 @@ int check_Babai (int kappa, F_mpz_mat_t B, double **mu, double **r, double *s,
       test = 0;
 
       loops++;
-      if (loops > 5){
+      if (loops > 1){
          return -1;
       }
             
