5191f90d7cafc488335462d37b682f545e19f511
Andy Novocin <andy@novocin.com>
2010-09-30 19:56:15 +0200

    trying fewer bits for large lattices

--- F_mpz_poly.c
+++ F_mpz_poly.c
@@ -8440,12 +8440,12 @@ int F_mpz_poly_factor_sq_fr_vHN(F_mpz_poly_factor_t final_fac, F_mpz_poly_factor
             num_entries++;
             if (M->r > 500){
                lll_start = clock();
-               newd = U_LLL_with_removal(M, 350L, B);
+               newd = U_LLL_with_removal(M, 50L, B);
                lll_stop = clock();
             }
             else if (M->r > 200){
                lll_start = clock();
-               newd = U_LLL_with_removal(M, 350L, B);
+               newd = U_LLL_with_removal(M, 50L, B);
                lll_stop = clock();
             }
             else if (M->r > 100){
@@ -8467,7 +8467,7 @@ int F_mpz_poly_factor_sq_fr_vHN(F_mpz_poly_factor_t final_fac, F_mpz_poly_factor
                abort();
             }
 
-            if ((col_cnt > 3) && (mix_data == 1))
+            if ((col_cnt > 4) && (mix_data == 1))
                mix_data = 0;
 
             F_mpz_mat_resize(M, newd, M->c);
