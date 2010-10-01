13ffd22c708a2eb3052a18f2e765a54a514ad136
William Hart <hart_wb@yahoo.com>
2010-10-01 15:02:24 +0100

    Added test for F_mpz_poly_scalar_abs.

--- F_mpz_poly-test.c
+++ F_mpz_poly-test.c
@@ -4946,6 +4946,87 @@ int test_F_mpz_poly_eval_horner_d_2exp()
    return result;
 }
 
+int test_F_mpz_poly_scalar_abs()
+{
+   mpz_poly_t m_poly, m_poly2;
+   F_mpz_poly_t F_poly, F_poly2;
+   int result = 1;
+   ulong bits, length, i;
+   mpz_t temp;
+   mpz_init(temp);
+   
+   mpz_poly_init(m_poly); 
+   mpz_poly_init(m_poly2); 
+   
+   ulong count1;
+   for (count1 = 0; (count1 < 20000) && (result == 1) ; count1++)
+   {
+      bits = z_randint(200)+ 1;
+      length = z_randint(200);  
+
+      F_mpz_poly_init(F_poly);
+      F_mpz_poly_init(F_poly2);
+      
+      mpz_randpoly(m_poly, length, bits); 
+      mpz_poly_to_F_mpz_poly(F_poly, m_poly);
+          
+      F_mpz_poly_scalar_abs(F_poly2, F_poly);
+          
+      F_mpz_poly_to_mpz_poly(m_poly2, F_poly2); 
+          
+      for (i = 0; i < m_poly->length; i++)
+      {
+         mpz_abs(temp, m_poly->coeffs[i]);
+         result &= (mpz_cmp(temp, m_poly2->coeffs[i]) == 0);
+      }
+
+		if (!result) 
+		{
+			printf("Error: length = %ld, bits = %ld\n", length, bits);
+         mpz_poly_print_pretty(m_poly, "x"); printf("\n");
+         mpz_poly_print_pretty(m_poly2, "x"); printf("\n");
+		}
+
+      F_mpz_poly_clear(F_poly2);
+      F_mpz_poly_clear(F_poly);
+   }
+   
+   // aliased abs
+	for (count1 = 0; (count1 < 20000) && (result == 1) ; count1++)
+   {
+      bits = z_randint(200)+ 1;
+      length = z_randint(200);        
+
+      F_mpz_poly_init(F_poly);
+      
+		mpz_randpoly(m_poly, length, bits); 
+      mpz_poly_to_F_mpz_poly(F_poly, m_poly);
+          
+      F_mpz_poly_scalar_abs(F_poly, F_poly);
+      F_mpz_poly_to_mpz_poly(m_poly2, F_poly); 
+          
+      for (i = 0; i < m_poly->length; i++)
+      {
+         mpz_abs(temp, m_poly->coeffs[i]);
+         result &= (mpz_cmp(temp, m_poly2->coeffs[i]) == 0);
+      }
+		
+		if (!result) 
+		{
+			printf("Error: length = %ld, bits = %ld\n", length, bits);
+         mpz_poly_print_pretty(m_poly, "x"); printf("\n");
+         mpz_poly_print_pretty(m_poly2, "x"); printf("\n");
+		}
+
+      F_mpz_poly_clear(F_poly);
+   }
+   
+   mpz_poly_clear(m_poly);
+   mpz_poly_clear(m_poly2);
+   
+   return result; 
+}
+
 void F_mpz_poly_test_all()
 {
    int success, all_success = 1;
@@ -4953,11 +5034,11 @@ void F_mpz_poly_test_all()
 
 #if TESTFILE
 #endif
-	/*
    RUN_TEST(F_mpz_poly_derivative); 
    RUN_TEST(F_mpz_poly_content); 
    RUN_TEST(F_mpz_poly_eval_horner_d); 
    RUN_TEST(F_mpz_poly_eval_horner_d_2exp); 
+   RUN_TEST(F_mpz_poly_scalar_abs); 
    RUN_TEST(F_mpz_poly_convert); 
    RUN_TEST(F_mpz_poly_getset_coeff_si); 
    RUN_TEST(F_mpz_poly_getset_coeff_ui); 
@@ -5006,7 +5087,7 @@ void F_mpz_poly_test_all()
 	RUN_TEST(F_mpz_poly_divexact); 
 	RUN_TEST(F_mpz_poly_pseudo_divrem_basecase); 
 	RUN_TEST(F_mpz_poly_pseudo_div_basecase); 
-   */RUN_TEST(F_mpz_poly_factor);
+   RUN_TEST(F_mpz_poly_factor);
    
    printf(all_success ? "\nAll tests passed\n" :
                         "\nAt least one test FAILED!\n");
