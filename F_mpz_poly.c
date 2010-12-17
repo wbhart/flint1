17481f109951f3b041a84a433e83c224100082bd
Andy Novocin <andy@novocin.com>
2010-12-17 21:38:57 +0100

    searching out the bug pt5

--- F_mpz_poly.c
+++ F_mpz_poly.c
@@ -8352,6 +8352,24 @@ int _F_mpz_poly_try_to_solve(int num_facs, ulong * part,
 
 #if TRACE
    printf(" not yet r = %ld\n", r);
+
+   printf("extra checks\n");
+
+   F_mpz_poly_fit_length(tryme, 1);
+   _F_mpz_poly_set_length(tryme, 1);
+   F_mpz_set_ui(tryme->coeffs + 0, 1);
+
+   F_mpz_poly_print(tryme); printf(" should be 1  1\n");
+
+   for (i = 0; i < lifted_fac->num_factors; i++){
+      F_mpz_poly_mul(tryme, tryme, lifted_fac->factors[i]);
+   }
+
+   F_mpz_poly_scalar_smod(tryme, tryme, P);
+
+   F_mpz_poly_sub(tryme, tryme, F);
+
+   F_mpz_poly_print(tryme); printf("if not 0 we have problems\n");
 #endif
  
    int i;
@@ -8405,24 +8423,6 @@ int _F_mpz_poly_try_to_solve(int num_facs, ulong * part,
 
 #if TRACE
    printf("nope not there trial_factors->num_factors = %ld\n", trial_factors->num_factors);
-
-   printf("extra checks\n");
-
-   F_mpz_poly_fit_length(tryme, 1);
-   _F_mpz_poly_set_length(tryme, 1);
-   F_mpz_set_ui(tryme->coeffs + 0, 1);
-
-   F_mpz_poly_print(tryme); printf(" should be 1  1\n");
-
-   for (i = 0; i < trial_factors->num_factors; i++){
-      F_mpz_poly_mul(tryme, tryme, trial_factors->factors[i]);
-   }
-
-   F_mpz_poly_scalar_smod(tryme, tryme, P);
-
-   F_mpz_poly_sub(tryme, tryme, F);
-
-   F_mpz_poly_print(tryme); printf("if not 0 we have problems\n");
 #endif
 
    F_mpz_poly_t f,Q,R;
