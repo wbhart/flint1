bf4e2eed3bcd30d3888b5a5a7db4dff1a047e22c
Andy Novocin <andy@novocin.com>
2010-12-18 00:11:57 +0100

    temporarily setting Hensel lifting to large amount for testing pt2

--- F_mpz_poly.c
+++ F_mpz_poly.c
@@ -8363,7 +8363,7 @@ int _F_mpz_poly_try_to_solve(int num_facs, ulong * part,
    {
       F_mpz_poly_fit_length(tryme, 1);
       _F_mpz_poly_set_length(tryme, 1);
-      F_mpz_set(tryme->coeffs + 0, temp_lc);
+      F_mpz_set(tryme->coeffs + 0, lc);
       
 	  int j;
       for (j = 0; j < r; j++)
